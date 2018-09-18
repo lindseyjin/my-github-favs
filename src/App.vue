149c4d97639023c601eb1e5d97ab6f89e984b509
Lindsey Jin <l44jin@uwaterloo.ca>
2018-09-18 01:49:04 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <content/>
+  </div>
+</template>
+
+<script>
+import Content from './components/content'
+
+export default {
+  name: 'App',
+  components: {Content}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'content',
+  data () {
+    return {
+      data: [{'name': 'sample', 'language': 'aaaa', 'latest_tag': 'eg'},
+        {'name': 'sample2', 'language': 'ssssss', 'latest_tag': 'eggg'},
+        {'name': 'pears github project', 'language': 'pear screms', 'latest_tag': 'AAAAA'}]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
